edd607b3e3f718c65c25d30bad5c6124bfeecc5a
Emre Ozturk <88751167+developer-emre@users.noreply.github.com>
2022-06-13 01:55:39 +0300

    Delete FAQProject directory

--- FAQProject/js/script.js
+++
@@ -1,8 +0,0 @@
-const questions = document.querySelectorAll(".question");
-
-
-questions.forEach(question => {
-    question.addEventListener("click",function(){
-        question.classList.toggle("active");
-    })
-});
